5d61052eed2d40de6029285575414d8d15aa5c9b
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-15 20:57:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do
     recipe = Recipe.create(:name => params["name"], :ingredients => params["ingredients"], :cook_time => params["cook_time"])
   end
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
 end
